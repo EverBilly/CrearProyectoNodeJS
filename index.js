5778c927827075057d25b8161d24bf43c00012a4
Ever Cifuentes <everbilly35@gmail.com>
2020-08-23 00:22:26 -0600

    ADD dependencies

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const mysql = require('mysql');
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.json());
 
-// Connexion to the Database
-const myConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Admin2020',
-    database: 'crud'
-});
+//Settings
+app.set('port', process.env.PORT || 30000);
 
-myConnection.connect((err) => {
-    if(!err){
-        console.log('Conexion Success');
-    }
-    else {
-        console.log('Conexion Failed \nError: ' + JSON.stringify(err));
-    }
+app.listen(app.get('port'), () => {
+    console.log('Conexion Stablish Port: ', app.get('port'));
 })
-
-app.listen(3000, () => console.log('Express Running'));
